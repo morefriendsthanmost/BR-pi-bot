642b4596dea83d0d006dfc71290012f96029ac6a
Jake Parkhouse <jparkhouse1511@gmail.com>
2019-05-31 00:48:58 +0100

    The very beginnings of the discord interface, with basic commands to say hello and shut down

---
+++ main.py
@@ -0,0 +1,21 @@
+import discord
+
+with open("passkeys.txt") as passfile:
+    token = passfile.read()
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("we have logged in as {0.user}".format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith("!hello"):
+        await message.channel.send("Hello!")
+    if message.content.startswith("!exit"):
+        await message.channel.send("Logging out")
+        await client.close()
+
+client.run(token)
